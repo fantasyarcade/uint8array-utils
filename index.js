d46a0f0ef80a0547d8dfd4f15329bed7648cc9b1
Jason Frame <jason@onehackoranother.com>
2018-05-05 14:13:41 +0100

    implement

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 exports.readFixedLengthAsciiString = function(array, offset, length) {
-	
+    let str = "";
+    for (let i = 0; i < length; ++i) {
+        const ch = array[offset + i];
+        if (ch === 0) {
+            break;
+        }
+        str += String.fromCharCode(ch);
+    }
+    return str;
 }
 
 exports.writeFixedLengthAsciiString = function(array, offset, length, str) {
-	
+    let i = 0;
+    const end = Math.min(length, str.length);
+    for (i = 0; i < end; ++i) {
+        array[offset + i] = String.charCodeAt(i);
+    }
+    for (; i < length; ++i) {
+        array[offset + i] = 0;
+    }
 }
